8cf3a78e66fbd40f951fe38257920f333b882ce6
Seif Moustafa <seif.moustafa516@gmail.com>
2024-03-19 05:47:13 +0200

    handle login view problem

--- lib/features/auth/login/widgets/login_view_body.dart
+++ lib/features/auth/login/widgets/login_view_body.dart
@@ -10,11 +10,13 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
+    return ListView(
+      children: const [
         TitleSection(),
-        AuthSection(),
+        Expanded(
+          // Wrap AuthSection with Expanded
+          child: AuthSection(),
+        ),
       ],
     );
   }
@@ -26,10 +28,9 @@ class TitleSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(
-          height: 112,
-        ),
+        SizedBox(height: 112),
         Padding(
           padding: EdgeInsets.only(left: 28),
           child: Text(
@@ -37,9 +38,7 @@ class TitleSection extends StatelessWidget {
             style: Styles.textStyle50,
           ),
         ),
-        SizedBox(
-          height: 83,
-        ),
+        SizedBox(height: 83),
         Padding(
           padding: EdgeInsets.only(left: 37),
           child: Text(
@@ -47,9 +46,7 @@ class TitleSection extends StatelessWidget {
             style: Styles.textStyle40,
           ),
         ),
-        SizedBox(
-          height: 24,
-        ),
+        SizedBox(height: 24),
       ],
     );
   }
@@ -61,88 +58,71 @@ class AuthSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String? email;
-
     String? password;
     GlobalKey<FormState> formkey = GlobalKey();
 
     return Form(
       key: formkey,
-      child: Expanded(
-        child: ListView(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 37),
-              child: Text(
-                'SIGN IN TO YOUR ACCOUNT',
-                style: Styles.textStyle20,
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 37),
+            child: Text(
+              'SIGN IN TO YOUR ACCOUNT',
+              style: Styles.textStyle20,
+            ),
+          ),
+          const SizedBox(height: 47),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: CustomFormTextField(
+              hintText: 'Email',
+              onChanged: (data) {
+                email = data;
+              },
+              obscureText: true,
+            ),
+          ),
+          const SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22),
+            child: CustomFormTextField(
+              hintText: 'Password',
+              onChanged: (data) {
+                password = data;
+              },
+              obscureText: true,
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Padding(
+            padding: EdgeInsets.only(right: 22.0),
+            child: Text(
+              'forgot your password?',
+              style: Styles.textStyle10,
+              textAlign: TextAlign.end,
+            ),
+          ),
+          const SizedBox(height: 39),
+          const CustomButton(buttonName: 'SIGN IN'),
+          const SizedBox(
+            height: 51,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Don\'t have an account?',
+                style: Styles.textStyle10.copyWith(fontWeight: FontWeight.w100),
               ),
-            ),
-            const SizedBox(
-              height: 47,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: CustomFormTextField(
-                hintText: 'Email',
-                onChanged: (data) {
-                  email = data;
-                },
-                obscureText: true,
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: CustomFormTextField(
-                hintText: 'Password',
-                onChanged: (data) {
-                  password = data;
-                },
-                obscureText: true,
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(right: 22.0),
-              child: Text(
-                'forgot your password?',
-                style: Styles.textStyle10,
-                textAlign: TextAlign.end,
-              ),
-            ),
-            const SizedBox(
-              height: 39,
-            ),
-            const CustomButton(buttonName: 'SIGN IN'),
-            const Expanded(
-              child: SizedBox(
-                height: 51,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an account?',
-                  style:
-                      Styles.textStyle10.copyWith(fontWeight: FontWeight.w100),
-                ),
-                Text(
-                  ' Create',
-                  style:
-                      Styles.textStyle10.copyWith(fontWeight: FontWeight.w800),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 89,
-            ),
-          ],
-        ),
+              Text(
+                ' Create',
+                style: Styles.textStyle10.copyWith(fontWeight: FontWeight.w800),
+              )
+            ],
+          ),
+          const SizedBox(height: 89),
+        ],
       ),
     );
   }
